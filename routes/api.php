e0e474a3ef8c1795545863fd0d16126a63379214
zainul abidin <bidingurusmk@gmail.com>
2024-03-09 19:47:14 +0700

    rev

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\MovieController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+header('Access-Control-Allow-Origin: *');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
