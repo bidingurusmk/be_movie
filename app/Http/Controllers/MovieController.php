c572e9d979400d2ef176ca5511a659f907a8977f
zainul abidin <bidingurusmk@gmail.com>
2024-03-10 08:24:51 +0700

    rev input

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Movie;
+use Validator;
 
 class MovieController extends Controller
 {
@@ -20,6 +21,38 @@ class MovieController extends Controller
                 'data'=>null,
             ]);
         }
-        
+    }
+    function insertMovie(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'voteaverage' => 'required',
+            'overview' => 'required',
+            'posterpath' => 'required|max:10000|mimes:jpg,jpeg,png'
+        ]);
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+        $file = request()->posterpath;
+        $imageName = time().'-'.$file->getClientOriginalName();
+        // $file->store('images', ['disk' => 'public_uploads']);
+        $uploadDir    = public_path().'/images';
+        $file->move($uploadDir, $imageName);
+        $movie = new Movie();
+        $movie->title = $request->title;
+        $movie->voteaverage = $request->voteaverage;
+        $movie->overview = $request->overview;
+        $movie->posterpath = $imageName;
+        try{   
+            $movie->save();
+            return Response()->json([
+                'status'=>true,
+                'message'=>'Sukses input data movie',
+            ]);
+        } catch(Exception $e){
+            return Response()->json([
+                'status'=>false,
+                'message'=>'Gagal input data movie',
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/getmovie',[MovieController::class,'getMovie']);
+Route::post('/insertmovie',[MovieController::class,'insertMovie']);
